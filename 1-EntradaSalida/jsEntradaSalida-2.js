09a837d4fe0a3c1816fc2d77ca7e5fb6ad5b2f97
thomii23 <thomiig23@gmail.com>
2017-02-03 01:23:02 +0200

    Primer Commit
    Hicimos el ej 1 y 2 en clase.

--- 1-EntradaSalida/jsEntradaSalida-2.js
+++ 1-EntradaSalida/jsEntradaSalida-2.js
@@ -1,7 +1,21 @@
 /*Debemos lograr tomar un nombre con 'prompt' 
 y luego mostrarlo por 'alert' al presionar el botón  'MOSTRAR'*/
+// Esta es la base datos del bloque http://www.w3schools.com/ // 
 function Mostar()
 {
+	// creacion de variables
+	// Promp (es una asignacion de datos) y la dif con alert
+	// es que el promp tiene un lugar para guardar datos.
+	var Nombre = "Thomas Guaita";
+	Nombre= prompt("Ingrese su nombre","Thomas Guaita");
+	alert (Nombre)
+
+
+
+
+
+
+	var person = prompt("Ingresa Tu Nombre", "Natalia Natalia");
 	
 }
 
