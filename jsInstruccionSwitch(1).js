f93c555de1969bb1422cafeff2552cdd37b72443
thomii23 <thomiig23@gmail.com>
2017-02-10 19:34:43 -0300

    switch 1 funcionando

---
+++ jsInstruccionSwitch(1).js
@@ -0,0 +1,21 @@
+function Mostrar()
+{
+//tomo la edad  
+var mesDelAño = document.getElementById('mes').value;
+
+switch(mesDelAño)
+{
+	case "Enero":
+	alert("Que comiences bien el año!!");break;
+	case "Marzo": 
+	alert("a clases");break;
+	case "Julio": 
+	alert("se vienen las vacaciones");break;
+	case "Diciembre": 
+	alert("felices fiestas");break;
+}
+ 
+
+
+
+}//FIN DE LA FUNCIÓN
